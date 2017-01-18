f0049af12de7d3b681ba17d0f46019073a71eea6
Utkarsh Tiwari <iamutkarshtiwari@gmail.com>
2017-01-18 23:41:01 +0530

    indentation fixed

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-public static void main(String[] args)throws IOException
-{
+public class Main {
+public static void main(String[] args)throws IOException {
 
-    long number = Long.parseLong(args[0]);
-    String decimals[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-    String places[] = {"", "", "hundred", "thousand", "lakh", "crore", "arab", "kharab"};
-    String integers[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", 
-    						"twelve", "thirteen", "fourteen", "fifteen",
-							"sixteen", "seventeen", "eighteen", "ninteen", "twenty"};
+  long number = Long.parseLong(args[0]);
+  String decimals[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+  String places[] = {"", "", "hundred", "thousand", "lakh", "crore", "arab", "kharab"};
+  String integers[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", 
+  												"twelve", "thirteen", "fourteen", "fifteen",
+													"sixteen", "seventeen", "eighteen", "ninteen", "twenty"};
 
 
 	String answer = "";
@@ -30,30 +28,28 @@ public static void main(String[] args)throws IOException
 			continue;
 		}
 
+		num = number % 100L;
+		number = number / 100L;
+		if (num == 0) {
+			i++;
+			continue;
+		}
 
-	num = number % 100L;
-	number = number / 100L;
-	if (num == 0) {
+		if (num < 21) {
+			// for 'teens' and single digited
+			if (i == 1)
+				temp = integers[Math.round(num)];
+			else
+				temp = integers[Math.round(num)] + " " + places[i];
+		} else {
+			temp = integers[Math.round(num % 10L)];
+			num = num / 10L;
+			temp = decimals[Math.round(num)] + " " + temp;
+			temp = temp + " " + places[i];
+		}
+
+		answer  = temp + " " + answer;
 		i++;
-		continue;
-	}
-
-
-	if (num < 21) {
-		// for 'teens' and single digited
-		if (i == 1)
-			temp = integers[Math.round(num)];
-		else
-			temp = integers[Math.round(num)] + " " + places[i];
-	} else {
-		temp = integers[Math.round(num % 10L)];
-		num = num / 10L;
-		temp = decimals[Math.round(num)] + " " + temp;
-		temp = temp + " " + places[i];
-	}
-
-	answer  = temp + " " + answer;
-	i++;
 	}
 	System.out.println();
 	System.out.println(answer);
